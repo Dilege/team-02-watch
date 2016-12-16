b565886c45c63336d5642c51066cae784a141b2f
likupf <likupf@gmail.com>
2016-12-16 11:48:22 +0100

    Mergeconflict

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,7 +6,8 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Team 2 +++++");
 		System.out.println("Hello Team 2 ++");
-
+		System.out.println("Hello Team 2 +++++");
+		System.out.println("Hello Team 2 ++");
 	}
 
 }
