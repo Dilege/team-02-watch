568b12f92219df0052d9a8deb0f84f0d9f5ea4d8
likupf <likupf@gmail.com>
2016-12-16 16:35:47 +0100

    Bitte keine Umlaute benutzen

--- src/main/java/Uhr/JFrameUhr.java
+++ src/main/java/Uhr/JFrameUhr.java
@@ -41,19 +41,19 @@ public class JFrameUhr extends JFrame {
 	public JFrameUhr() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		setTitle("The Watch"); 
+		setTitle("The Watch");
 		setLocationRelativeTo(null);
 
-		// Menüzeile (JMenuBar) erzeugen und in das Fenster (JFrame) einfügen
+		// Menuezeile (JMenuBar) erzeugen und in das Fenster (JFrame) einfuegen
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 
-		// Menü (konfig) erzeugen und in die Menüzeile (JMenuBar) einfügen
+		// Menue (konfig) erzeugen und in die Menuezeile (JMenuBar) einfuegen
 		JMenu konfig = new JMenu("Einstellungen");
 		menuBar.add(konfig);
 
-		// Menüeinträge (BackgroundColorItem) erzeugen und dem Menü (JMenu)
-		// "Konfiguration" hinzufügen
+		// Menueeintraege (BackgroundColorItem) erzeugen und dem Menue (JMenu)
+		// "Konfiguration" hinzufuegen
 		JMenuItem BackgroundColorItem = new JMenuItem("Hintergrundfarbe");
 		BackgroundColorItem.addActionListener(new ActionListener() {
 
@@ -63,8 +63,8 @@ public class JFrameUhr extends JFrame {
 		});
 		konfig.add(BackgroundColorItem);
 
-		// Menüeinträge (writingColorItem) erzeugen und dem Menü (JMenu)
-		// "Schriftfarbe" hinzufügen
+		// Menueeintraege (writingColorItem) erzeugen und dem Menue (JMenu)
+		// "Schriftfarbe" hinzufuegen
 		JMenuItem writingColorItem = new JMenuItem("Schriftfarbe");
 		writingColorItem.addActionListener(new ActionListener() {
 
@@ -74,8 +74,8 @@ public class JFrameUhr extends JFrame {
 		});
 		konfig.add(writingColorItem);
 
-		// Menüeinträge (writingSize) erzeugen und dem Menü (JMenu)
-		// "Schriftfarbe" hinzufügen
+		// Menueeinträge (writingSize) erzeugen und dem Menue (JMenu)
+		// "Schriftfarbe" hinzufuegen
 		JMenuItem writingSize = new JMenuItem("Schriftgroesse");
 		writingSize.addActionListener(new ActionListener() {
 
@@ -88,19 +88,18 @@ public class JFrameUhr extends JFrame {
 
 		final JButton startButton = new JButton("START");
 		final JButton stoppButton = new JButton("STOPP");
-		
+
 		startButton.setBounds(10, 206, 202, 23);
 		getContentPane().add(startButton);
 		startButton.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
-				startButton.setText("Go!");
+				startButton.setText("GO!");
 				stoppButton.setText("STOPP");
-				Starten.start(); 
+				Starten.start();
 			}
 		});
 
-		
 		stoppButton.setBounds(229, 206, 195, 23);
 		getContentPane().add(stoppButton);
 		stoppButton.addActionListener(new ActionListener() {
@@ -108,21 +107,18 @@ public class JFrameUhr extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				startButton.setText("START");
 				stoppButton.setText("finish");
-				Stoppen.stopp(); 
-				
+				Stoppen.stopp();
+
 			}
 		});
-		
-		
+
 		JLabel lblWatchOurWatch = new JLabel("Watch our Watch!");
 		lblWatchOurWatch.setBounds(10, 11, 120, 23);
 		getContentPane().add(lblWatchOurWatch);
-		
+
 		JLabel lblNewLabel = new JLabel("New label");
 		lblNewLabel.setBounds(74, 85, 285, 68);
 		getContentPane().add(lblNewLabel);
-		
-	
-		
+
 	}
 }
