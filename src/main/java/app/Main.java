800bc6c92a7ed3a9de979cef126cff6168c3e9f3
vianney NORMANT <vianney NORMANT@10.30.250.9>
2016-12-16 11:42:46 +0100

    test1

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		System.out.println("Hello Team 2 +++");
 		System.out.println("Hello Team 2 +++++");
 		System.out.println("Hello Team 2 ++");
-		System.out.println("Hello Team 2 ++");
+		System.out.println("Hello");
 	}
 
 }
