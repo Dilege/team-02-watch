9ddf72c3553334ec0118b022cb2705bd0b62ce08
tobia <tobia@10.30.249.249>
2016-12-16 11:41:58 +0100

    Test Merge Conflict

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		System.out.println("Hello Team 2 +++++");
 		System.out.println("Hello Team 2 ++");
 		System.out.println("Hello Team 2 ++");
+		System.out.println("Hello Vianney");
 	}
 
 }
