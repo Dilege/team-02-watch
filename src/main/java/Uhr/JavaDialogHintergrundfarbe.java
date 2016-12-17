654c8eb6f4a56b003e27bf38911fa1c4064e512e
tobia <tobia@DESKTOP-QCT32HM.home>
2016-12-17 22:16:28 +0100

    Aktueller Stand JavaDialog

---
+++ src/main/java/Uhr/JavaDialogHintergrundfarbe.java
@@ -0,0 +1,98 @@
+package Uhr;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.colorchooser.*;
+ 
+/* ColorChooserDemo.java requires no other files. */
+public class JavaDialogHintergrundfarbe extends JPanel
+                              implements ChangeListener {
+ 
+    protected static JColorChooser tcc;
+    protected JLabel banner;
+ 
+    public JavaDialogHintergrundfarbe() {
+        super(new BorderLayout());
+ 
+        //Set up the banner at the top of the window
+        banner = new JLabel("Wählen Sie eine Hintergrundfarbe",
+                            JLabel.CENTER);
+        banner.setForeground(Color.black);
+        banner.setBackground(Color.red);
+        banner.setOpaque(true);
+        banner.setFont(new Font("SansSerif", Font.BOLD, 24));
+        banner.setPreferredSize(new Dimension(100, 65));
+        
+ 
+        JPanel bannerPanel = new JPanel(new BorderLayout());
+        bannerPanel.add(banner, BorderLayout.CENTER);
+        bannerPanel.setBorder(BorderFactory.createTitledBorder(""));
+ 
+        //Set up color chooser for setting text color
+        tcc = new JColorChooser(banner.getBackground());
+        tcc.setPreviewPanel(new JPanel());
+        tcc.getSelectionModel().addChangeListener(this);
+        tcc.setBorder(BorderFactory.createTitledBorder(
+                                             "Mögliche Schriftfarben"));
+        ColorSelectionModel model = tcc.getSelectionModel();
+        model.addChangeListener(new ChangeListener() {
+          public void stateChanged(ChangeEvent evt) {
+            ColorSelectionModel model = (ColorSelectionModel) evt.getSource();
+            Color newColor = model.getSelectedColor();
+            System.out.println(model.getSelectedColor());
+          }
+        });
+
+        // Get current selected color
+        Color color = tcc.getColor();
+ 
+        add(bannerPanel, BorderLayout.CENTER);
+        add(tcc, BorderLayout.PAGE_END);
+    }
+ 
+    public void stateChanged(ChangeEvent e) {
+        ColorSelectionModel model = (ColorSelectionModel) e.getSource();
+        Color newColor = model.getSelectedColor();
+        banner.setBackground(newColor);
+      }
+   
+    
+
+        //Color newColor = tcc.getColor();
+       
+        
+    
+ 
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Schriftfarbe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Create and set up the content pane.
+        JComponent newContentPane = new JavaDialogHintergrundfarbe();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            Color color = tcc.getColor();
+           System.out.println(color);
+            }
+        });
+    }
+}
