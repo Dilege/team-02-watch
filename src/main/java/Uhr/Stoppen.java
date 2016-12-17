dea1306a392711b2eaa0e519f00827cdf16623ce
vianney NORMANT <vianney NORMANT@bob.home>
2016-12-17 20:38:58 +0100

    stopp Funktional

--- src/main/java/Uhr/Stoppen.java
+++ src/main/java/Uhr/Stoppen.java
@@ -1,5 +1,6 @@
 package Uhr;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Calendar;
@@ -9,8 +10,17 @@ import javax.swing.Timer;
 
 public class Stoppen extends Gui{
 
+	private static final long serialVersionUID = 1L;
+
 	public static void stopp(){
 		
+		 // UhrText Einstellungen
+		timeF = new JTextField(10);
+		timeF.setVisible(true);
+		timeF.setBounds(74, 61, 290, 80);
+		timeF.setFont(new Font("Arial", Font.HANGING_BASELINE, 48));
+        timeF.setEditable(false);
+        
 		Timer t = new Timer(1000, new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
