d4e4adfe1bab02a17a0c7d4f72c6b5446884e041
vianney NORMANT <vianney NORMANT@bob.home>
2016-12-17 17:19:17 +0100

    Aufräumen Klassen

--- src/main/java/Uhr/JFrameUhr.java
+++ src/main/java/Uhr/JFrameUhr.java
@@ -1,6 +1,5 @@
 package Uhr;
 
-import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,105 +9,81 @@ import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
-
 @SuppressWarnings("serial")
 public class JFrameUhr extends JFrame {
 	public static JTextField timeF;
 
-
 	public JFrameUhr() {
+
+		// Frame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		setTitle("The Watch");
 		setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
 
-		// Menuezeile (JMenuBar) erzeugen und in das Fenster (JFrame) einfuegen
+		// Menu Einstellungen
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-
-		// Menue (konfig) erzeugen und in die Menuezeile (JMenuBar) einfuegen
 		JMenu konfig = new JMenu("Einstellungen");
 		menuBar.add(konfig);
 
-		// Menueeintraege (BackgroundColorItem) erzeugen und dem Menue (JMenu)
-		// "Konfiguration" hinzufuegen
+		// Unter-Menu Hintergrundfarbe
 		JMenuItem BackgroundColorItem = new JMenuItem("Hintergrundfarbe");
 		BackgroundColorItem.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Hintergrundfarbe auswaehlen");
 			}
 		});
 		konfig.add(BackgroundColorItem);
 
-		// Menüeinträge (writingColorItem) erzeugen und dem Menü (JMenu)
-		// "Schriftfarbe" hinzufügen
+		// Unter-Menu Schriftfarbe
 		JMenuItem writingColorItem = new JMenuItem("Schriftfarbe");
 		writingColorItem.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Schriftfarbe auswaehlen");
 			}
 		});
 		konfig.add(writingColorItem);
 
-		// Menüeinträge (writingSize) erzeugen und dem Menü (JMenu)
-		// "Schriftfarbe" hinzufügen
+		// Unter-Menu Schriftgroesse
 		JMenuItem writingSize = new JMenuItem("Schriftgroesse");
 		writingSize.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Schriftgroesse auswaehlen");
 			}
 		});
 		konfig.add(writingSize);
-		getContentPane().setLayout(null);
 
+		// START- STOPP Buttons
 		final JButton startButton = new JButton("START");
 		final JButton stoppButton = new JButton("STOPP");
-		
+
+		// START Button
 		startButton.setBounds(10, 206, 202, 23);
 		getContentPane().add(startButton);
-		
 		startButton.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent e) {
 				startButton.setText("Go!");
 				stoppButton.setText("STOPP");
 				Starten start = new Starten();
 				start.start();
 				getContentPane().add(timeF);
-				
-				
-				
-				
 			}
 		});
 
-		
+		// STOPP Button
 		stoppButton.setBounds(229, 206, 195, 23);
 		getContentPane().add(stoppButton);
 		stoppButton.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent e) {
 				startButton.setText("START");
 				stoppButton.setText("finish");
-				Stoppen.stopp(); 
-				
+				Stoppen.stopp();
+
 			}
 		});
-		
-		
-		
-		
 
 	}
-	/*public void start(){
-	timeF = new JTextField(10);
-	timeF.setBounds(74, 61, 290, 80);
-	timeF.setFont(new Font("Arial", Font.HANGING_BASELINE, 48));
-	
-	timeF.setEditable(false);
-	Clock.clock();
-	}*/
+
 }
