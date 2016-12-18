62135142a9d2ef95f7597510fdfa7cd342b5c451
Mohamed <Mohamed@192.168.1.79>
2016-12-18 13:12:28 +0100

    FarbeMemory

---
+++ src/main/java/Test/Dialog.java
@@ -0,0 +1,104 @@
+package Test;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+// Import-Anweisung f�r unser JLabel 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Dialog extends Memory {
+
+	Color ausgewaehlteFarbe;
+	JLabel lab = new JLabel("Beispiel JLabel");
+	JFrame f = new JFrame();
+	JButton hinterground = new JButton();
+	JButton schrift = new JButton();
+
+	// main-Methode
+	public static void main(String[] args) {
+		new Dialog();
+	}
+
+	public Dialog() {
+
+		frm = f;
+		f.setTitle("Mein JDialog Beispiel");
+		f.setBounds(200, 200, 400, 300);
+		try {
+			restoreFile(f);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		Color farbeH =hcolor;
+		f.getContentPane().setBackground(farbeH);
+		lab.setBounds(100, 100, 100, 40);
+		lab.setForeground(scolor);
+		f.add(lab);
+		f.setLayout(null);
+		settingButton();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+	public void chooseColorHinterground() {
+		ausgewaehlteFarbe = JColorChooser.showDialog(null, "Farbauswahl", null);
+		Color c = ausgewaehlteFarbe;
+		hintergrundFarbe = c;
+		f.getContentPane().setBackground(c);
+	}
+
+	public void chooseColorSchrift() {
+		ausgewaehlteFarbe = JColorChooser.showDialog(null, "Farbauswahl", null);
+		Color c = ausgewaehlteFarbe;
+		schriftFarbe = c;
+		lab.setForeground(c);
+
+	}
+
+	public void settingButton() {
+		hinterground.setBounds(320, 10, 20, 20);
+		Container contentPane = f.getContentPane();
+		contentPane.add(hinterground);
+		f.add(hinterground);
+		schrift.setBounds(350, 10, 20, 20);
+		contentPane.add(schrift);
+		f.add(schrift);
+		ListenderHinterground();
+		ListenderSchrift();
+		
+	}
+	public void ListenderHinterground(){
+		hinterground.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				chooseColorHinterground();
+				event();
+
+			}
+		});
+		
+		hintergrundFarbe=hcolor;
+		
+	}
+	public void ListenderSchrift(){
+		schrift.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				chooseColorSchrift();
+				event();
+
+			}
+		});
+		schriftFarbe=scolor;
+	}
+}
---
+++ src/main/java/Test/Memory.java
@@ -0,0 +1,95 @@
+package Test;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class Memory {
+	// Variable declaration
+	static Color hintergrundFarbe;
+	static Color schriftFarbe;
+	static Color hcolor;
+	static Color scolor;
+	static File file = new File("Color.txt");
+	static Properties p = new Properties();
+	static JFrame frm;
+
+	public static void storeFile(Frame f1) throws IOException {
+        //Hintergroundfarben in den Variablen hr,hg und hb speichern
+		int hr = hintergrundFarbe.getRed();
+		int hg = hintergrundFarbe.getGreen();
+		int hb = hintergrundFarbe.getBlue();
+		//Schriftfarben in den Variablen sr,sg und sb speichern
+		int sr = schriftFarbe.getRed();
+		int sg = schriftFarbe.getGreen();
+		int sb = schriftFarbe.getBlue();
+		//Die ganzen Variablen in den "Properties p " speichern
+		p.setProperty("hr", "" + hr);
+		p.setProperty("hg", "" + hg);
+		p.setProperty("hb", "" + hb);
+		p.setProperty("sr", "" + sr);
+		p.setProperty("sg", "" + sg);
+		p.setProperty("sb", "" + sb);
+		//Die Properties in den file Color.txt schreiben
+		BufferedWriter color = new BufferedWriter(new FileWriter(file));
+		p.store(color, "FarbEigenschaften");
+	}
+
+	public static void restoreFile(JFrame f2) throws IOException {
+		//Einlesen der Properties
+		BufferedReader color = new BufferedReader(new FileReader(file));
+		p.load(color);
+
+		// Schreibt die eingelesenen Properties in den Variablen hr,hg,hb,sr,sg und sb
+		int hr = Integer.parseInt(p.getProperty("hr"));
+		int hg = Integer.parseInt(p.getProperty("hg"));
+		int hb = Integer.parseInt(p.getProperty("hb"));
+		int sr = Integer.parseInt(p.getProperty("sr"));
+		int sg = Integer.parseInt(p.getProperty("sg"));
+		int sb = Integer.parseInt(p.getProperty("sb"));
+		//Mit dem Variablen zwei Color Objekt schreiben
+		hcolor = new Color(hr, hg, hb);
+		scolor = new Color(sr, sg, sb);
+
+	}
+
+	public static void event() {
+		frm.addWindowListener(new WindowAdapter() {
+
+			public void windowClosing(WindowEvent we) {
+
+				try {
+					storeFile(frm);
+				} catch (Exception e) {
+
+					e.printStackTrace();
+				}
+
+				System.exit(0);
+			}
+		});
+
+		File colorFile = new File("Color.txt");
+		if (colorFile.exists()) {
+			try {
+				restoreFile(frm);
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+
+		} else {
+			frm.setLocationByPlatform(true);
+		}
+	}
+}
