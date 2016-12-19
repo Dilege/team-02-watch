f38ef0bd006418d26a26f96a62f29cddd06de5d6
likupf <likupf@gmail.com>
2016-12-19 13:43:10 +0100

    Analoguhr

---
+++ src/main/java/analog/Main.java
@@ -0,0 +1,16 @@
+package analog;
+
+
+
+public class Main  {
+   
+    public static void 
+               main(String[]args){
+      
+       SetUp set = new SetUp("Analog Watch", 400);
+       set.start();
+     
+    }
+   
+
+}
